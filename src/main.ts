dbd7b558d7d82e503bceb6bb4d6ba031a09c1e4e
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-10-02 12:09:08 +0700

    Config Versioning

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { VersioningType } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //versioning
+  app.setGlobalPrefix('api')
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1'
+  })
   const configService = app.get(ConfigService)
   await app.listen(configService.get<string>("PORT"));
 }
