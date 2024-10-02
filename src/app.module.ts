fa2950a75af730d58dc689b0c7027da072fefc81
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-10-02 13:05:56 +0700

    Build Module User and Connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true})],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}), 
+    UsersModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
