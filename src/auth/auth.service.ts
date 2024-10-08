87f27a24783949f5624f937345503e1d9d21fb7b
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-10-08 15:45:41 +0700

    Save refresh token to database

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -22,12 +22,16 @@ export class AuthService {
         const {id, role, fullName} = user 
         const payload = {sub: id, email, role, fullName}
         const token = await this.generateAuthToken(payload);
+        //save refreshToken to user
+        await this.userService.update(id, {refreshToken: token.refresh_token})
         return {
             _id: id,
             ...token,
             tokenExpires: this.configService.get<string>('JWT_ACCESS_EXPIRE')
         } 
     }
+    
+    
     async generateAuthToken(payload: any): Promise<TokenResDto> {
         return {
             access_token: await this.jwtService.signAsync(payload),
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,5 +1,9 @@
 import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsString } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(OmitType(CreateUserDto,['password'] as const)) {
+
+    @IsString()
+    refreshToken: string;
 }
