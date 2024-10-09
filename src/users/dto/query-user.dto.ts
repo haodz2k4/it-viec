4176842483d70bf44c61a686cc411bfad8085eb7
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-10-09 21:49:43 +0700

    Update filter user dto

--- src/users/dto/query-user.dto.ts
+++ src/users/dto/query-user.dto.ts
@@ -1,17 +1,9 @@
+import { PickType } from "@nestjs/mapped-types";
 import { Transform, Type } from "class-transformer";
-import { IsEnum, IsIn, IsNumber, IsOptional, IsString } from "class-validator";
+import { IsEnum, IsNumber, IsOptional, IsString } from "class-validator";
 import { SortOrder } from "src/utils/types/sort.type";
-export class FilterUserDto {
-     //Filter 
-     @IsString()
-     @IsOptional()
-     @IsIn(["active","inactive"])
-     status?: string;
- 
-     @IsString()
-     @IsOptional()
-     @IsIn(["user","role"])
-     role?: string;
+import { CreateUserDto } from "./create-user.dto";
+export class FilterUserDto extends PickType(CreateUserDto,["role","status"] as const) {
 }
 export class QueryUserDto extends FilterUserDto {
 
