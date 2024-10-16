60366803830d44d51ed9f70f602780423915f133
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-10-16 16:27:32 +0700

    company schema

--- src/companies/schema/company.schema.ts
+++ src/companies/schema/company.schema.ts
@@ -1,1 +1,22 @@
-export class Company {}
+import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+
+export type companyDocument = HydratedDocument<Company>;
+@Schema({timestamps: true})
+export class Company {
+
+    @Prop({required: true})
+    name: string;
+
+    @Prop()
+    description: string;
+
+    @Prop()
+    logo: string;
+
+    @Prop()
+    address: string;
+}
+
+export const CompanySchema = SchemaFactory.createForClass(Company);
+
