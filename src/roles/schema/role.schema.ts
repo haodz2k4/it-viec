24d446b5fe64666390184e34d6e45b325b9485a9
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-10-18 14:53:42 +0700

    Role Schema

--- src/roles/schema/role.schema.ts
+++ src/roles/schema/role.schema.ts
@@ -1,1 +1,21 @@
-export class Role {}
+import { Schema, Prop } from "@nestjs/mongoose";
+
+@Schema({
+    timestamps: true 
+})
+export class Role {
+    
+    @Prop({required: true})
+    title: string;
+
+    @Prop()
+    description: string;
+
+    @Prop({type: [String], default: []})
+    permissions: string[]
+
+    @Prop({
+        default: false
+    })
+    deleted: boolean;
+}
